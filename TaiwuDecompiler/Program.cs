747a4a62fa884906515b1ce336933af8f54ff8cb
Wenxuan Zhao <viz@linux.com>
2018-10-17 22:55:28 -0700

    实现从注册表中读取游戏安装位置

--- TaiwuDecompiler/Program.cs
+++ TaiwuDecompiler/Program.cs
@@ -1,14 +1,45 @@
-﻿namespace TaiwuDecompiler
+﻿using System;
+using TaiwuDecompiler.Utilities;
+
+namespace TaiwuDecompiler
 {
+    /// <summary>
+    /// 反编译器主类。
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 太吾绘卷的 Steam 游戏 ID，用于通过注册表获取游戏路径。
+        /// </summary>
+        private const int STEAM_GAME_ID = 838350;
+
+        /// <summary>
+        /// 反编译器入口方法。
+        /// </summary>
+        /// <param name="args">参数列表（未使用）</param>
         static void Main(string[] args)
         {
-            // TODO: 通过注册表寻找游戏路径（参考 SMAPI）
+            string gamePath = getSteamGameInstallLocation(STEAM_GAME_ID);
+            Console.Error.WriteLine(string.Format("Game Path: {0}", gamePath)); // FIXME: Debug
             // TODO: 计算 Assembly-CSharp.dll 的 SHA256 值
             // TODO: 脱壳 Assembly-CSharp.dll，并存至 Assembly-CSharp-Unpacked-<SHA256>.dll
             // TODO: 调用 dnspy（使用参数：--spaces 2 --no-tokens）去反编译 Assembly-CSharp-Unpacked-<SHA256>.dll，并输出到临时目录
             // TODO: 清理非游戏文件
+            Console.ReadKey(); // FIXME: Debug
+        }
+
+        /// <summary>
+        /// 从注册表获取特定 Steam 游戏的安装路径。
+        /// </summary>
+        /// <param name="gameId">Steam 游戏 ID</param>
+        /// <returns>安装路径</returns>
+        private static string getSteamGameInstallLocation(uint gameId)
+        {
+            string steamGameRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App " + gameId;
+            string steamGameInstallLocationName = "InstallLocation";
+
+            string installLocation = RegistryUtility.LocalMachine.GetString(steamGameRegistryKey, steamGameInstallLocationName);
+            return installLocation;
         }
     }
 }
---
+++ TaiwuDecompiler/Utilities/RegistryUtility.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+
+namespace TaiwuDecompiler.Utilities
+{
+    /// <summary>
+    /// 注册表实用工具。
+    /// </summary>
+    class RegistryUtility
+    {
+        /// <summary>
+        /// 从注册表中读取键值。
+        /// </summary>
+        /// <typeparam name="T">值的类型</typeparam>
+        /// <param name="baseKey">基础分支</param>
+        /// <param name="subKey">子项</param>
+        /// <param name="name">名称</param>
+        /// <returns>对应键的值</returns>
+        public static T GetValue<T>(RegistryKey baseKey, string subKey, string name)
+        {
+            RegistryKey fullKey = baseKey.OpenSubKey(subKey);
+            if (fullKey == null) return default(T);
+            return (T)fullKey.GetValue(name);
+        }
+
+        /// <summary>
+        /// 注册表中本机项的实用工具。
+        /// </summary>
+        public static class LocalMachine
+        {
+            /// <summary>
+            /// 从注册表中读取本机项的键值。
+            /// </summary>
+            /// <typeparam name="T">值的类型</typeparam>
+            /// <param name="subKey">子项</param>
+            /// <param name="name">名称</param>
+            /// <returns>对应键的值</returns>
+            public static T GetValue<T>(string subKey, string name)
+            {
+                RegistryKey localMachine = Registry.LocalMachine;
+                return RegistryUtility.GetValue<T>(localMachine, subKey, name);
+            }
+
+            /// <summary>
+            /// 从注册表中读取本机项的字符串键值。
+            /// </summary>
+            /// <param name="key">子项</param>
+            /// <param name="name">名称</param>
+            /// <returns>对应键的值</returns>
+            public static string GetString(string key, string name)
+            {
+                return GetValue<string>(key, name);
+            }
+        }
+    }
+}
