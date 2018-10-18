1cdb9009aedb4126a24055a2c5d192a70d8c2737
Wenxuan Zhao <viz@linux.com>
2018-10-18 13:29:40 -0700

    对 Assembly-CSharp.dll 脱壳并保存

--- TaiwuDecompiler/Program.cs
+++ TaiwuDecompiler/Program.cs
@@ -35,7 +35,12 @@ namespace TaiwuDecompiler
             string originalAssemblyPath = getAssemblyPath(gamePath);
             string originalAssemblyHash = HashUtility.GetFileSHA256(originalAssemblyPath);
             Console.Error.WriteLine(string.Format("Assembly 散列: {0}", originalAssemblyHash)); // FIXME: Debug
-            // TODO: 脱壳 Assembly-CSharp.dll，并存至 Assembly-CSharp-Unpacked-<SHA256>.dll
+            string monoLibraryPath = getMonoPath(gamePath);
+            byte[] unpackedAssemblyData = AssemblyUtility.LoadImage(monoLibraryPath, originalAssemblyPath);
+            Console.Error.WriteLine(string.Format("脱壳后的 Assembly 大小: {0}", unpackedAssemblyData.Length)); // FIXME: Debug
+            string unpackedAssemblyPath = originalAssemblyPath.Replace(ORIGINAL_FILENAME, string.Format(UNPACKED_FILENAME, originalAssemblyHash));
+            Console.Error.WriteLine(string.Format("保存脱壳后的 Assembly 到: {0}", unpackedAssemblyPath)); // FIXME: Debug
+            File.WriteAllBytes(unpackedAssemblyPath, unpackedAssemblyData);
             // TODO: 调用 dnspy（使用参数：--spaces 2 --no-tokens）去反编译 Assembly-CSharp-Unpacked-<SHA256>.dll，并输出到临时目录
             // TODO: 清理非游戏文件
             Console.ReadKey(); // FIXME: Debug
@@ -64,11 +69,29 @@ namespace TaiwuDecompiler
         {
             const string MANAGED_DIR = "Managed";
             string[] matches = Directory.GetDirectories(baseDirectory, MANAGED_DIR, SearchOption.AllDirectories);
-            if (matches.Length != 1) {
+            if (matches.Length != 1)
+            {
                 string message = matches.Length == 0 ? "没有找到 {0} 目录" : "找到多个 {0} 目录";
                 throw new Exception(string.Format(message, MANAGED_DIR));
             }
             return Path.Combine(matches[0], ORIGINAL_FILENAME);
+        }
+
+        /// <summary>
+        /// 获取 Mono 动态链接库的路径。
+        /// </summary>
+        /// <param name="baseDirectory">搜索基础路径</param>
+        /// <returns>Mono 动态链接库的路径</returns>
+        private static string getMonoPath(string baseDirectory)
+        {
+            const string MONO_LIBRARY_NAME = "mono.dll";
+            string[] matches = Directory.GetFiles(baseDirectory, MONO_LIBRARY_NAME, SearchOption.AllDirectories);
+            if (matches.Length != 1)
+            {
+                string message = matches.Length == 0 ? "没有找到 {0}" : "找到多个 {0}";
+                throw new Exception(string.Format(message, MONO_LIBRARY_NAME));
+            }
+            return matches[0];
         }
     }
 }
---
+++ TaiwuDecompiler/Utilities/AssemblyUtility.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace TaiwuDecompiler.Utilities
+{
+    class AssemblyUtility
+    {
+        /// <summary>
+        /// MonoImage 结构体。
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        private struct MonoImage
+        {
+            public int refCount;
+            public IntPtr rawDataHandle;
+            public IntPtr rawData;
+            public int rawDataLen;
+        }
+
+        /// <summary>
+        /// Mono 的 Assembly 镜像加载函数。
+        /// </summary>
+        /// <param name="data">储存数据的指针</param>
+        /// <param name="dataLen ">数据长度</param>
+        /// <param name="needCopy">是否需要拷贝</param>
+        /// <param name="status">储存加载状态的指针</param>
+        /// <param name="refOnly">是否仅供引用</param>
+        /// <param name="name">Assembly 的文件名</param>
+        /// <returns></returns>
+        [DllImport("mono.dll", EntryPoint = "mono_image_open_from_data_with_name", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr MonoOpenImage(
+            IntPtr data,
+            uint dataLen,
+            bool needCopy,
+            IntPtr status,
+            bool refOnly,
+            string name
+        );
+
+        /// <summary>
+        /// 初始化 Mono 镜像列表。
+        /// </summary>
+        [DllImport("mono.dll", EntryPoint = "mono_images_init")]
+        internal static extern void MonoInitImages();
+
+        /// <summary>
+        /// Win32 的库加载函数。
+        /// </summary>
+        /// <param name="dllPath">要加载的 DLL 路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllPath);
+
+        /// <summary>
+        /// 使用 Mono 加载 Assembly 的镜像。
+        /// </summary>
+        /// <param name="monoPath">Mono 库的路径</param>
+        /// <param name="assemblyPath">Assembly 镜像的路径</param>
+        /// <returns>加载了数据的数组</returns>
+        public static byte[] LoadImage(string monoPath, string assemblyPath)
+        {
+            IntPtr pData = IntPtr.Zero;
+
+            try
+            {
+                // 准备可供调用 Unmanaged 代码的带有 Assembly 数据的指针
+                byte[] assemblyData = File.ReadAllBytes(assemblyPath);
+                pData = Marshal.AllocHGlobal(assemblyData.Length);
+                Marshal.Copy(assemblyData, 0, pData, assemblyData.Length);
+
+                // 加载 Mono 动态链接库并加载加壳过的 Assembly
+                LoadLibrary(monoPath);
+                MonoInitImages();
+                var pImage = MonoOpenImage(pData, (uint)assemblyData.Length, false, IntPtr.Zero, false, assemblyPath);
+                MonoImage loadedImage = (MonoImage)Marshal.PtrToStructure(pImage, typeof(MonoImage));
+
+                // 准备用于储存 Managed 数据的数组，并写入已加载的数据
+                byte[] dumpedData = new byte[loadedImage.rawDataLen];
+                Marshal.Copy(loadedImage.rawData, dumpedData, 0, loadedImage.rawDataLen);
+                return dumpedData;
+            }
+            finally
+            {
+                if (pData != IntPtr.Zero) Marshal.FreeHGlobal(pData);
+            }
+
+            // FIXME: 检查 MonoImage 是否有被释放？
+        }
+    }
+}
