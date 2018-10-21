a89edf3f470f264afe37554845ca96096b2abc4e
Wenxuan Zhao <viz@linux.com>
2018-10-20 20:59:39 -0700

    使用 ILSpy 对 Assembly-CSharp.dll 进行反编译
    * 修复了方法的命名规范
    * 将目标平台设置为 .NET 4.6 以兼容 ILSpy
    + 添加 System.Reflection.Metadata 的 NuGet 包以兼容 ILSpy
    + 将 ILSpy 项目 Fork 的分支暂时添加至本项目，直到 ILSpy PR 被 Merge

--- TaiwuDecompiler/Program.cs
+++ TaiwuDecompiler/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.CSharp;
+using ICSharpCode.Decompiler.CSharp.OutputVisitor;
+using ICSharpCode.Decompiler.Metadata;
+using System;
 using System.IO;
 using TaiwuDecompiler.Utilities;
 
@@ -17,12 +21,12 @@ namespace TaiwuDecompiler
         /// <summary>
         /// 原始的 Assembly 文件名。
         /// </summary>
-        private const string ORIGINAL_FILENAME = "Assembly-CSharp.dll";
+        private const string ORIGINAL_FILENAME = "Assembly-CSharp";
 
         /// <summary>
         /// 脱壳后的 Assembly 文件名。
         /// </summary>
-        private const string UNPACKED_FILENAME = "Assembly-CSharp-{0}.dll";
+        private const string UNPACKED_FILENAME_TEMPLATE = "Assembly-CSharp-{0}";
 
         /// <summary>
         /// 反编译器入口方法。
@@ -30,20 +34,28 @@ namespace TaiwuDecompiler
         /// <param name="args">参数列表（未使用）</param>
         static void Main(string[] args)
         {
-            string gamePath = getSteamGameInstallLocation(STEAM_GAME_ID);
-            Console.Error.WriteLine(string.Format("游戏路径: {0}", gamePath)); // FIXME: Debug
-            string originalAssemblyPath = getAssemblyPath(gamePath);
+            string gamePath = GetSteamGameInstallLocation(STEAM_GAME_ID);
+            Console.WriteLine(string.Format("使用游戏路径: {0}", gamePath));
+
+            string originalAssemblyPath = GetAssemblyPath(gamePath);
             string originalAssemblyHash = HashUtility.GetFileSHA256(originalAssemblyPath);
-            Console.Error.WriteLine(string.Format("Assembly 散列: {0}", originalAssemblyHash)); // FIXME: Debug
-            string monoLibraryPath = getMonoPath(gamePath);
+            Console.WriteLine(string.Format("游戏 Assembly 散列: {0}", originalAssemblyHash));
+
+            string monoLibraryPath = GetMonoPath(gamePath);
             byte[] unpackedAssemblyData = AssemblyUtility.LoadImage(monoLibraryPath, originalAssemblyPath);
-            Console.Error.WriteLine(string.Format("脱壳后的 Assembly 大小: {0}", unpackedAssemblyData.Length)); // FIXME: Debug
-            string unpackedAssemblyPath = originalAssemblyPath.Replace(ORIGINAL_FILENAME, string.Format(UNPACKED_FILENAME, originalAssemblyHash));
-            Console.Error.WriteLine(string.Format("保存脱壳后的 Assembly 到: {0}", unpackedAssemblyPath)); // FIXME: Debug
+            string UNPACKED_FILENAME = string.Format(UNPACKED_FILENAME_TEMPLATE, originalAssemblyHash);
+            string unpackedAssemblyPath = originalAssemblyPath.Replace(ORIGINAL_FILENAME, UNPACKED_FILENAME);
+            Console.WriteLine(string.Format("写入脱壳后的 Assembly 到: {0}", unpackedAssemblyPath));
             File.WriteAllBytes(unpackedAssemblyPath, unpackedAssemblyData);
-            // TODO: 调用 dnspy（使用参数：--spaces 2 --no-tokens）去反编译 Assembly-CSharp-Unpacked-<SHA256>.dll，并输出到临时目录
+
+            string decompiledDirectory = Path.Combine(gamePath, "TaiwuDecompiler-" + UNPACKED_FILENAME);
+            Console.WriteLine("\n开始反编译……");
+            DecompileAssembly(unpackedAssemblyPath, decompiledDirectory);
+
             // TODO: 清理非游戏文件
-            Console.ReadKey(); // FIXME: Debug
+            Console.WriteLine(string.Format("完成！反编译源码已保存至：{0}", decompiledDirectory));
+            Console.WriteLine("按任意键退出……");
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -51,7 +63,7 @@ namespace TaiwuDecompiler
         /// </summary>
         /// <param name="gameId">Steam 游戏 ID</param>
         /// <returns>安装路径</returns>
-        private static string getSteamGameInstallLocation(uint gameId)
+        private static string GetSteamGameInstallLocation(uint gameId)
         {
             string steamGameRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App " + gameId;
             string steamGameInstallLocationName = "InstallLocation";
@@ -65,7 +77,7 @@ namespace TaiwuDecompiler
         /// </summary>
         /// <param name="baseDirectory">搜索基础路径</param>
         /// <returns>原始 Assembly 的路径</returns>
-        private static string getAssemblyPath(string baseDirectory)
+        private static string GetAssemblyPath(string baseDirectory)
         {
             const string MANAGED_DIR = "Managed";
             string[] matches = Directory.GetDirectories(baseDirectory, MANAGED_DIR, SearchOption.AllDirectories);
@@ -74,7 +86,7 @@ namespace TaiwuDecompiler
                 string message = matches.Length == 0 ? "没有找到 {0} 目录" : "找到多个 {0} 目录";
                 throw new Exception(string.Format(message, MANAGED_DIR));
             }
-            return Path.Combine(matches[0], ORIGINAL_FILENAME);
+            return Path.Combine(matches[0], ORIGINAL_FILENAME + ".dll");
         }
 
         /// <summary>
@@ -82,7 +94,7 @@ namespace TaiwuDecompiler
         /// </summary>
         /// <param name="baseDirectory">搜索基础路径</param>
         /// <returns>Mono 动态链接库的路径</returns>
-        private static string getMonoPath(string baseDirectory)
+        private static string GetMonoPath(string baseDirectory)
         {
             const string MONO_LIBRARY_NAME = "mono.dll";
             string[] matches = Directory.GetFiles(baseDirectory, MONO_LIBRARY_NAME, SearchOption.AllDirectories);
@@ -92,6 +104,23 @@ namespace TaiwuDecompiler
                 throw new Exception(string.Format(message, MONO_LIBRARY_NAME));
             }
             return matches[0];
+        }
+
+        /// <summary>
+        /// 使用 ILSpy 反编译 Assembly。
+        /// </summary>
+        /// <param name="assemblyPath">Assembly 路径</param>
+        /// <param name="outputPath">输出路径</param>
+        private static void DecompileAssembly(string assemblyPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath)) Directory.Delete(outputPath, true);
+            Directory.CreateDirectory(outputPath);
+
+            WholeProjectDecompiler decompiler = new WholeProjectDecompiler();
+
+            var module = new PEFile(assemblyPath);
+            decompiler.AssemblyResolver = new UniversalAssemblyResolver(assemblyPath, false, module.Reader.DetectTargetFrameworkId(assemblyPath));
+            decompiler.DecompileProject(module, outputPath);
         }
     }
 }
