6de460efa26c092b35abaae5dbdc91aa94b1a87b
Wenxuan Zhao <viz@linux.com>
2018-11-19 10:10:19 +0800

    对于需要的文件使用 Unity 工程的默认路径而不是进行路径搜索
    + 在游戏路径搜索失败时允许使用当前路径的上级目录作为游戏路径

--- TaiwuDecompiler/Program.cs
+++ TaiwuDecompiler/Program.cs
@@ -1,8 +1,7 @@
-﻿using ICSharpCode.Decompiler;
-using ICSharpCode.Decompiler.CSharp;
-using ICSharpCode.Decompiler.CSharp.OutputVisitor;
+﻿using ICSharpCode.Decompiler.CSharp;
 using ICSharpCode.Decompiler.Metadata;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using TaiwuDecompiler.Utilities;
 
@@ -17,6 +16,16 @@ namespace TaiwuDecompiler
         /// 太吾绘卷的 Steam 游戏 ID，用于通过注册表获取游戏路径。
         /// </summary>
         private const int STEAM_GAME_ID = 838350;
+
+        /// <summary>
+        /// 游戏目录名。
+        /// </summary>
+        readonly static string GAME_DIRECTORY = "The Scroll Of Taiwu";
+
+        /// <summary>
+        /// 游戏文件名。
+        /// </summary>
+        readonly static string GAME_NAME = $"{GAME_DIRECTORY} Alpha V1.0";
 
         /// <summary>
         /// 原始的 Assembly 文件名。
@@ -34,14 +43,22 @@ namespace TaiwuDecompiler
         /// <param name="args">参数列表（未使用）</param>
         static void Main(string[] args)
         {
-            string gamePath = GetSteamGameInstallLocation(STEAM_GAME_ID);
-            Console.WriteLine($"使用游戏路径: {gamePath}");
+            (string gamePath, string originalAssemblyPath, string monoLibraryPath) = GetSteamGameInstallation(STEAM_GAME_ID);
 
-            string originalAssemblyPath = GetAssemblyPath(gamePath);
+            if (gamePath == null)
+            {
+                Console.Error.WriteLine("错误：搜索游戏路径失败，请将反编译器目录拷贝至游戏路径后重试。");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine($"使用游戏路径: {gamePath}");
+            Console.WriteLine($"使用 Assembly: {originalAssemblyPath}");
+            Console.WriteLine($"使用 Mono 库: {monoLibraryPath}");
+            Console.WriteLine();
+
             string originalAssemblyHash = HashUtility.GetFileSHA256(originalAssemblyPath);
             Console.WriteLine($"游戏 Assembly 散列: {originalAssemblyHash}");
 
-            string monoLibraryPath = GetMonoPath(gamePath);
             byte[] unpackedAssemblyData = AssemblyUtility.LoadImage(monoLibraryPath, originalAssemblyPath);
             string UNPACKED_FILENAME = string.Format(UNPACKED_FILENAME_TEMPLATE, originalAssemblyHash);
             string unpackedAssemblyPath = originalAssemblyPath.Replace(ORIGINAL_FILENAME, UNPACKED_FILENAME);
@@ -62,17 +79,44 @@ namespace TaiwuDecompiler
         }
 
         /// <summary>
-        /// 从注册表获取特定 Steam 游戏的安装路径。
+        /// 获取通过 Steam 所安装的特定 Unity 游戏路径。
         /// </summary>
-        /// <param name="gameId">Steam 游戏 ID</param>
-        /// <returns>安装路径</returns>
-        private static string GetSteamGameInstallLocation(uint gameId)
+        /// <param name="gameId">Steam 游戏 ID（可为空）</param>
+        /// <returns>安装路径、Assembly 路径、Mono 库路径</returns>
+        private static (string, string, string) GetSteamGameInstallation(uint gameId = 0)
         {
             string steamGameRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App " + gameId;
             string steamGameInstallLocationName = "InstallLocation";
 
-            string installLocation = RegistryUtility.LocalMachine.GetString(steamGameRegistryKey, steamGameInstallLocationName);
-            return installLocation;
+            string installLocationFromRegistry = RegistryUtility.LocalMachine.GetString(steamGameRegistryKey, steamGameInstallLocationName);
+            string currentLocation = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), ".."));
+
+            List<string> installationLocationCandidatesList = new List<string> {
+                installLocationFromRegistry,
+                @"C:\Program Files (x86)\Steam\steamapps\common\The Scroll Of Taiwu",
+                @"C:\Program Files\Steam\steamapps\common\The Scroll Of Taiwu",
+                currentLocation,
+            };
+
+            foreach (string installationLocationCandidate in installationLocationCandidatesList)
+            {
+                try
+                {
+                    string assemblyPathCandidate = GetAssemblyPath(installationLocationCandidate);
+                    string monoPathCandidate = GetMonoPath(installationLocationCandidate);
+
+                    if (File.Exists(assemblyPathCandidate) && File.Exists(monoPathCandidate))
+                    {
+                        return (installationLocationCandidate, assemblyPathCandidate, monoPathCandidate);
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return (null, null, null);
         }
 
         /// <summary>
@@ -82,14 +126,7 @@ namespace TaiwuDecompiler
         /// <returns>原始 Assembly 的路径</returns>
         private static string GetAssemblyPath(string baseDirectory)
         {
-            const string MANAGED_DIR = "Managed";
-            string[] matches = Directory.GetDirectories(baseDirectory, MANAGED_DIR, SearchOption.AllDirectories);
-            if (matches.Length != 1)
-            {
-                string message = matches.Length == 0 ? "没有找到 {0} 目录" : "找到多个 {0} 目录";
-                throw new Exception(string.Format(message, MANAGED_DIR));
-            }
-            return Path.Combine(matches[0], ORIGINAL_FILENAME + ".dll");
+            return $@"{baseDirectory}\{GAME_NAME}_Data\Managed\{ORIGINAL_FILENAME}.dll";
         }
 
         /// <summary>
@@ -99,14 +136,7 @@ namespace TaiwuDecompiler
         /// <returns>Mono 动态链接库的路径</returns>
         private static string GetMonoPath(string baseDirectory)
         {
-            const string MONO_LIBRARY_NAME = "mono.dll";
-            string[] matches = Directory.GetFiles(baseDirectory, MONO_LIBRARY_NAME, SearchOption.AllDirectories);
-            if (matches.Length != 1)
-            {
-                string message = matches.Length == 0 ? "没有找到 {0}" : "找到多个 {0}";
-                throw new Exception(string.Format(message, MONO_LIBRARY_NAME));
-            }
-            return matches[0];
+            return $@"{baseDirectory}\Mono\EmbedRuntime\mono.dll";
         }
 
         /// <summary>
